ccc14878331ecc159af71dae8ff1b99123611e8a
rebecca burch <rebecca.burch.stu@gmail.com>
2021-04-13 01:52:25 -0500

    error messages

--- server/controllers/cards.js
+++ server/controllers/cards.js
@@ -3,23 +3,43 @@ const Card = require('../models/cards');
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
-    .catch(() => res.status(500).send({ message: 'Error' }));
+    .catch((err) => {
+      const ERROR_CODE = 500;
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).json(
+          {
+            message: 'Internal Server Error',
+          },
+        );
+      }
+      return res.send({ message: 'something went wrong' });
+    });
 };
 
 module.exports.createCard = (req, res) => {
   const {
-    likes, name, link, owner, createdAt,
+    likes, name, link, createdAt,
   } = req.body;
 
   Card.create({
     likes,
     name,
     link,
-    owner,
+    owner: req.user._id,
     createdAt,
-  }).populate(['likes', 'owners'])
+  })
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Error' }));
+    .catch((err) => {
+      const ERROR_CODE = 500;
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).json(
+          {
+            message: 'Internal Server Error',
+          },
+        );
+      }
+      return res.send({ message: 'something went wrong' });
+    });
 };
 
 module.exports.getCardId = (req, res) => {
@@ -30,5 +50,73 @@ module.exports.getCardId = (req, res) => {
       }
       return res.status(404).json({ message: 'Card ID not found' });
     })
-    .catch(() => res.status(500).json({ message: 'Internal Server Error' }));
+    .catch((err) => {
+      const ERROR_CODE = 500;
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).json(
+          {
+            message: 'Internal Server Error',
+          },
+        );
+      }
+      return res.send({ message: 'something went wrong' });
+    });
+};
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // add _id to the array if it's not there yet
+    {
+      new: true,
+    },
+  )
+    .then((card) => {
+      if (card) {
+        return res.send(card);
+      }
+      return res.status(404).json({ message: 'Card ID not found' });
+    })
+    .catch((err) => {
+      const ERROR_CODE = 500;
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).json(
+          {
+            message: 'Internal Server Error',
+          },
+        );
+      }
+      return res.send({ message: 'something went wrong' });
+    });
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    {
+      $pull: {
+        likes: req.user._id,
+      },
+    }, // remove _id from the array
+    {
+      new: true,
+    },
+  )
+    .then((card) => {
+      if (card) {
+        return res.send(card);
+      }
+      return res.status(404).json({ message: 'Card ID not found' });
+    })
+    .catch((err) => {
+      const ERROR_CODE = 500;
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).json(
+          {
+            message: 'Internal Server Error',
+          },
+        );
+      }
+      return res.send({ message: 'something went wrong' });
+    });
 };
